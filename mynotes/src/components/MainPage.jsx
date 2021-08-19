1ce398c4524b1a2e65ce4108aa0fceb16a514a45
ahmet yanik <ahmetyank4242@gmail.com>
2021-08-20 01:35:58 +0200

    styles are added

--- mynotes/src/components/MainPage.jsx
+++ mynotes/src/components/MainPage.jsx
@@ -4,40 +4,52 @@ import background from "../images/cool-background.png";
 import NotesStick from "./NoteStick";
 
 function MainPage() {
+  const [notes, setNotes] = useState([]);
 
-    const [notes,setNotes]=useState([]);
+  console.log(notes);
 
-   
+  function addData(e) {
+    var newData = document.getElementById("input").value;
+
+    setNotes(function (oldDatas) {
+      return [...oldDatas, newData];
+    });
+
+    document.getElementById("input").value="";
+
+    e.preventDefault();
+  }
+
+  function deleteData(e){
+
+    var element = e.target["id"];
+    var deletingElement = document.getElementById(element);
+
+    var ahmet = notes.shift()
+
+    setNotes(function(öncekiveriler){
+      
+      return[
+      ahmet]
+    });
+
+
+    
 
     console.log(notes);
+  }
 
-
-    function addData(e){
-
-        var newData = document.getElementById("input").value;
-
-        setNotes(function(oldDatas){
-          return [...oldDatas,newData];
-        });
-
-
-        e.preventDefault();
-
-    }
 
   return (
     <div
       style={{
         backgroundImage: `url(${background})`,
         backgroundRepeat: "no-repeat",
-        minHeight:"100vh",
-        minWidth:"100vw",
+        minHeight: "100vh",
+        minWidth: "100vw",
         backgroundSize: "cover",
       }}
     >
-
-    
-
       <h1 className="title text-center pt-3 pb-3">My Notes</h1>
 
       <div className="searchbox">
@@ -50,23 +62,32 @@ function MainPage() {
             aria-label="Search"
             name="note"
           />
-          <button onClick={addData} className="btn btn-outline-success" type="submit">
+          <button
+            onClick={addData}
+            className="btn btn-outline-success"
+            type="submit"
+          >
             Add
           </button>
         </form>
       </div>
 
       <div className="top-notes">
-      
-      <div className="notes">
-        {notes.map(function(note,id){
-          return(
-            <div>{id+1}-{note}.</div>
-          )
-        })}  
+        <div  className="notes">
+          {notes.map(function (note, id) {
+            return (
+              <div   className="d-flex align-items-center ">
+                <div>
+                  <span id={id}>
+                    {id + 1}-{note}
+                  </span>
+                </div>
+                  <i onClick={deleteData} id={id} class="fas fa-trash icons"></i>
+              </div>
+            );
+          })}
+        </div>
       </div>
-
-      </div>     
 
     </div>
   );
