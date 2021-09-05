ef5364faf4b06e4c92c40384b5eaf88dc401308c
ahmet yanik <ahmetyank4242@gmail.com>
2021-09-05 12:36:07 +0200

    ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import MainPage from './components/MainPage';
+import React, { useState } from "react";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addData(e) {
+    var newData = document.getElementById("input").value;
+    var alertElement = document.getElementById("alert");
+
+    if (newData.length > 0) {
+      alertElement.style.display = "none";
+      setNotes(function (oldDatas) {
+        return [...oldDatas, newData];
+      });
+    } else {
+      alertElement.style.display = "block";
+    }
+
+    document.getElementById("input").value = "";
+  }
+
+  function deleteData(e) {
+    var element = e.target["id"];
+
+    
+    console.log(element);
+    const temp = [...notes];
+    temp.splice(element, 1);
+
+    setNotes(temp)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <MainPage addData={addData} deleteData={deleteData} notes={notes} />
+
     </div>
   );
 }
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import background from "../images/cool-background.png";
+import NotesStick from "./NoteStick";
+
+function MainPage({addData, deleteData, notes}) {
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: "no-repeat",
+        minHeight: "100vh",
+        minWidth: "100vw",
+        backgroundSize: "cover",
+      }}
+    >
+      <h1 className="title text-center pt-3 pb-3">My Notes</h1>
+
+      <div className="searchbox flex-column align-items-center">
+        <div>
+          <div className="d-flex">
+            <input
+              id="input"
+              className="form-control me-2"
+              type="search"
+              placeholder="Add Notes"
+              aria-label="Search"
+              name="note"
+            />
+            <button
+              onClick={addData}
+              className="btn btn-outline-success"
+              type="submit"
+            >
+              Add
+            </button>
+          </div>
+          <div className="title pt-1 px-2">
+            {notes.length > 0 ? (
+              <div className="titel">Total Notes:{notes.length}</div>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      </div>
+
+      <div className="d-flex justify-content-center">
+        <div
+          id="alert"
+          className="alert alert-warning mt-2"
+          role="alert"
+          style={{ display: "none" }}
+        >
+          <strong>:)</strong> You should write something!
+        </div>
+      </div>
+      <div className="top-notes">
+        <div className="notes">
+          {notes.map(function (note, id) {
+            return (
+              <NotesStick delete={deleteData} key={id} note={note} id={id} />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
